fc68f1b9eb72203980bd38a6031b27f533641394
João Marco de Paula Santana <Joo Marco de Paula Santana>
2015-11-29 19:18:51 -0200

    Model of unit tests

--- tests/unit-tests/controllers/LoginControllerSpec.js
+++ tests/unit-tests/controllers/LoginControllerSpec.js
@@ -1,6 +1,35 @@
 describe('LoginController', function() {
 
-    var controller;
+    var controller,
+    state,
+    scope;
 
     beforeEach(module('socketiochat'));
+
+    beforeEach(inject(function($controller, $rootScope, $state){
+        scope = $rootScope.$new();
+        state = $state;
+        controller = $controller('LoginController', {
+            $scope: scope,
+            $state: state
+        });
+    }));
+
+    describe('LoginController', function() {
+        it('should go to chat page', function() {
+
+        });
+
+        it('should not go to chat page if identification is null', function() {
+
+        });
+
+        it('should not go to chat page if identification is undefined', function() {
+
+        });
+
+        it('should not go to chat page if identification is empty', function() {
+            
+        });
+    });
 });
